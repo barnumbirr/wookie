2f1710eab8acf72ec697b3eae7713beddd1c0e3d
grm34 <fripouillejack@gmail.com>
2014-12-14 22:15:13 +0100

    mixed spaces and tabs

--- wookie/config.py
+++ wookie/config.py
@@ -8,14 +8,14 @@ __date__    = "2012 - 2014"
 __license__ = "Apache v2.0 License"
 
 network = {
-	'server': 'irc.example.net',
-	'port': 6667,
-	'SSL': False,
-	'ipv6': False,
-	'channels': ['#channel'],
-	'bot_nick': 'wookie',
-	'bot_name': 'wookie v.3.0 is available at https://github.com/c0ding/wookie',
-	'password': ''
+    'server': 'irc.example.net',
+    'port': 6667,
+    'SSL': False,
+    'ipv6': False,
+    'channels': ['#channel'],
+    'bot_nick': 'wookie',
+    'bot_name': 'wookie v.3.0 is available at https://github.com/c0ding/wookie',
+    'password': ''
 }
 
 feeds = {
@@ -25,9 +25,9 @@ feeds = {
 }
 
 wookie = {
-	'bot_owner': ['maybe_you_!'],
-	'path': '/path/to/wookie/dir/',
-	'start_bot': 'screen -dmS wookie',
-	'kill_bot': 'screen -X -S wookie kill',
-	'mode': '' # Please, leave this empty !
+    'bot_owner': ['maybe_you_!'],
+    'path': '/path/to/wookie/dir/',
+    'start_bot': 'screen -dmS wookie',
+    'kill_bot': 'screen -X -S wookie kill',
+    'mode': '' # Please, leave this empty !
 }
