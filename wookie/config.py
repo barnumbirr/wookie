f1f009c88fb1d803adb8123ae1303969a48d8cf9
grm34 <fripouillejack@gmail.com>
2014-12-14 22:21:41 +0100

    apply pep8

--- wookie/config.py
+++ wookie/config.py
@@ -3,18 +3,19 @@
 
 __appname__ = "wookie"
 __version__ = "v.3.0"
-__author__  = "@c0ding, @grm34"
-__date__    = "2012 - 2014"
+__author__ = "@c0ding, @grm34"
+__date__ = "2012 - 2014"
 __license__ = "Apache v2.0 License"
 
 network = {
-    'server': 'irc.example.net',
+    'server': 'irc.bakaserv.net',
     'port': 6667,
     'SSL': False,
     'ipv6': False,
-    'channels': ['#channel'],
+    'channels': ['#test'],
     'bot_nick': 'wookie',
-    'bot_name': 'wookie v.3.0 is available at https://github.com/c0ding/wookie',
+    'bot_name': 'wookie v.3.0 is available at '
+                'https://github.com/c0ding/wookie',
     'password': ''
 }
 
@@ -29,5 +30,5 @@ wookie = {
     'path': '/path/to/wookie/dir/',
     'start_bot': 'screen -dmS wookie',
     'kill_bot': 'screen -X -S wookie kill',
-    'mode': '' # Please, leave this empty !
+    'mode': ''
 }
