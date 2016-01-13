028f126b803fb81682f5da1cb3287dda7965ab14
grm34 <fripouillejack@gmail.com>
2016-01-13 01:28:30 +0100

    wookie-v.3.2 @grm34

--- wookie/__init__.py
+++ wookie/__init__.py
@@ -3,6 +3,6 @@
 
 __appname__ = "wookie"
 __version__ = "v.3.2"
-__author__  = "@c0ding, @grm34"
-__date__    = "2012...2016"
+__author__ = "@c0ding, @grm34"
+__date__ = "2012...2016"
 __license__ = "Apache v2.0 License"
